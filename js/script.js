c6ee72f01223d8b1ce2b9ea81461c195e1217f9f
Ricardo Rauch <ricardo@gravityonmars.com>
2012-03-20 19:42:23 -0300

    fixes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 $(function() {
 	$(".dropdown a.selected").click(function() {
+		$('.create-room').show().next(".text").hide();
 	  $(this).toggleClass("active");
-		$(this).next(".dropdown-options").fadeToggle('fast');
+		$(this).next(".dropdown-options").toggle();
 	});
+	
+
 	
 	$(".create-room").click(function() {
 		$(this).hide();
 		$(this).next(".text").fadeIn();
 	});
+	
+	$(".lock").click(function() {
+		$(this).toggleClass('active');
+	});
+	
+	$(".fancybox").fancybox({'margin': 0, 'padding': 0});
+	
 });
